ca9539522fc3416790595af09c40c505247f2258
sasz94 <sacha.bartlomiej@gmail.com>
2020-03-14 18:30:25 +0100

    table created with fetched data

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyTable from '../MyTable/MyTable'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <MyTable/>
+    </div>
+  );
+}
+
+export default App;
